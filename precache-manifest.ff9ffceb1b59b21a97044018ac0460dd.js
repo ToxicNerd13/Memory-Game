7a3af763e97cd634ac12207510fdcbb49f84297e
Killoran Lehenbauer <k.lehenbauer13@gmail.com>
2020-01-14 15:19:50 -0700

    Updates

---
+++ precache-manifest.ff9ffceb1b59b21a97044018ac0460dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970f90abde6d68f417a5b6849a8a874e",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "965b3770e335cee01a15",
+    "url": "/memory-game/static/css/main.fd12f8b1.chunk.css"
+  },
+  {
+    "revision": "3f239b1393d2b56d2356",
+    "url": "/memory-game/static/js/2.9ca99d07.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/memory-game/static/js/2.9ca99d07.chunk.js.LICENSE"
+  },
+  {
+    "revision": "965b3770e335cee01a15",
+    "url": "/memory-game/static/js/main.8e667da3.chunk.js"
+  },
+  {
+    "revision": "ce0b70db5dd49f9a97f0",
+    "url": "/memory-game/static/js/runtime-main.82ba3a20.js"
+  }
+]);
